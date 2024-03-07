644c081cfc0a6059b408661c00722becd60335f5
Stephen Karanja <karanjastephenn@gmail.com>
2024-03-07 19:01:18 +0300

    users route

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,8 @@ const router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.send('Users not yet implemented: Just chillax.');
+  res.send('All good.');
 });
 
 module.exports = router;
