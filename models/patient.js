244bdd6da91eb33455db6d2405de3c6777799293
Stephen Karanja <karanjastephenn@gmail.com>
2024-03-08 07:39:45 +0300

    patient schema

--- models/patient.js
+++ models/patient.js
@@ -1,12 +1,14 @@
 const mongoose = require("mongoose");
 
+const { DateTime } = require("luxon");
+
 const Schema = mongoose.Schema;
 
 const PatientSchema = new Schema({
   first_name: { type: String, required: true, maxLength: 100 },
   surname: { type: String, required: true, maxLength: 100 },
   date_of_birth: { type: Date },
-  patient_history: { type: String },
+  patient_history: { type: Schema.Types.Mixed },
   national_id: { type: Number, required: true },
   chronic_illness: { type: String },
 });
@@ -27,6 +29,10 @@ PatientSchema.virtual("name").get(function () {
 PatientSchema.virtual("url").get(function () {
   // don't use arrow function coz the this object is needed
   return `/archive/patient/${this._id}`;
+});
+
+PatientSchema.virtual("date_birth_formatted").get(function () {
+  return DateTime.fromJSDate(this.date_of_birth).toLocaleString(DateTime.DATE_MED);
 });
 
 // Export model
